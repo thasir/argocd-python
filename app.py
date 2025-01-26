cdd50ea498190a5bacd94e9304668465f6202c5c
thasir <ahmedshaik93@gmail.com>
2025-01-26 19:24:18 +0530

    count=2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    greeting_message = "Hello, This Python Flask app is being deployed using Github Actions and ArgoCD to EKS Cluster count=1"
+    greeting_message = "Hello, This Python Flask app is being deployed using Github Actions and ArgoCD to EKS Cluster count=2"
     return render_template('index.html', message=greeting_message)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
